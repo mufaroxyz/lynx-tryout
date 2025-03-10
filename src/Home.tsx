9cfd08f11778bb4decd52c9ee5683873984b99d8
Xazu <Xazu001@users.noreply.github.com>
2025-03-10 17:45:55 +0100

    RR

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from '@lynx-js/react';
+import { useState } from '@lynx-js/react';
 import { useNavigate } from 'react-router';
 
 export default function Index() {
@@ -8,31 +8,22 @@ export default function Index() {
 
   return (
     <view className="bg-bg pt-site h-full">
-      <text className="p-4 font-bold text-alt-1 text-5xl">App!</text>
+      <text className="p-4 font-bold text-5xl text-accent-1">Home!</text>
       <view className="section">
         <view className="container">
           <text class="text-2xl">{count}</text>
         </view>
       </view>
-      <view
-        className="section"
-        bindtap={() => {
-          setCount(count + 1);
-        }}
-      >
+      <view className="section" bindtap={() => setCount(count + 1)}>
         <view className="flex container">
           <text class="px-6 py-2 rounded-full text-2xl bg-accent-1">+1</text>
         </view>
       </view>
-      <view className="section" bindtap={() => navigate('/home')}>
+      <view className="section" bindtap={() => navigate('/')}>
         <view className="flex justify-center container">
-          <text class="bg-alt-1 px-6 py-2 rounded-full text-2xl">Go</text>
+          <text class="px-6 py-2 rounded-full text-2xl bg-accent-1">Go</text>
         </view>
       </view>
     </view>
   );
-}
-
-function bgOnly() {
-  console.log('Test');
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,18 @@
 import { root } from '@lynx-js/react';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import './tailwind.css?url';
 
-import { App } from './App.js';
+import App from './App.jsx';
+import Home from './Home.jsx';
 
-root.render(<App />);
+root.render(
+  <MemoryRouter>
+    <Routes>
+      <Route path="/" element={<App />} index />
+      <Route path="/home" element={<Home />} index />
+    </Routes>
+  </MemoryRouter>,
+);
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept();
